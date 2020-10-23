e6925988795e866492ba69ad65999101d97b6c98
SylvainMarsili <sylvain.marsili@etu.univ-cotedazur.fr>
2020-10-23 09:37:06 +0200

    ajout enum drink

--- fr.univcotedazur.polytech.si4.fsm.project/src/drink/Drink.java
+++ fr.univcotedazur.polytech.si4.fsm.project/src/drink/Drink.java
@@ -1,11 +1,20 @@
 package drink;
 
-public class Drink {
+public enum Drink {
+	COFFE("coffee",0.35f),
+	EXPRESSO("Expresso", 0.50f),
+	TEA("Tea", 0.40f);
 	private String name;
 	private float price;
-	
-	public Drink(String n,float p){
+	Drink(String n,float p){
 		this.name= n;
 		this.price=p;
+	}
+	
+	public float getPrice() {
+		return price;
+	}
+	public String getName() {
+		return name;
 	}
 }
